a19b69145d03d9aae4512c39c16c5349b558264f
rafaelMartins1922 <rafael.martins@poli.ufrj.br>
2021-12-16 01:50:05 -0300

    mudando arquivos de teste

--- trabalho_1/Java/src/main/constants/GraphFiles.java
+++ trabalho_1/Java/src/main/constants/GraphFiles.java
@@ -1,5 +1,5 @@
 package main.constants;
 
 public class GraphFiles {
-    public static final String[] fileNames = {"grafo_1.txt", "grafo_2.txt", "grafo_3.txt", "grafo_4.txt", "grafo_5.txt", "grafo_6.txt"};
+    public static final String[] fileNames = {"exemplo_aula4_bfs.txt", "exemplo_aula5_dfs.txt", "edge_list_1000.txt", "grafo_4.txt", "grafo_5.txt", "grafo_6.txt"};
 }
